0600edd6d863ea777f7998c40a45c7cc01881da4
Augusto Developer <aug_oliv@hotmail.com>
2022-05-26 10:41:06 -0300

    nextjs

--- .next/static/chunks/pages/demos-0549ea7e11093ec3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7528],{9440:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/demos",function(){return u(4350)}])}},function(n){n.O(0,[1664,5675,94,9547,8511,4350,9774,2888,179],(function(){return _=9440,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
--- next.config.js
+++ next.config.js
@@ -45,3 +45,14 @@ module.exports = {
       },
     },
   }
+  const withPWA = require('next-pwa')
+ 
+  module.exports = withPWA({
+    pwa: {
+      disable: process.env.NODE_ENV === 'development',
+      register: true,
+      scope: '/app',
+      sw: 'service-worker.js',
+      //...
+    }
+  })
